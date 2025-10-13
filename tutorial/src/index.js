a63a4ba823acbc2602fd133d75e2790147aa6baf
Adham Mustafa <adhoomy.mustafa@gmail.com>
2025-10-13 01:29:07 -0400

    Add book list component with styling

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -3,34 +3,45 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 
+const books = [
+  {
+    id: 1,
+    title: 'The Encompassing Epistle: Al-Risalah al-Jami\'ah',
+    author: 'Aḥmed ibn Zayn Al-Ḥabashī',
+    image: 'https://m.media-amazon.com/images/I/71jltELj4YL._AC_UY327_FMwebp_QL65_.jpg',
+  },
+  {
+    id: 2,
+    title: 'The Accessible Conspectus',
+    author: 'Musa Furber',
+    image: 'https://m.media-amazon.com/images/I/313eYpZ7pfL._SS135_.jpg',
+  },
+];
+
 function BookList() {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  const { title, author, image } = props;
+
   return (
     <article className='book'>
-      <Image></Image>
-      <Title />
-      <Author />
+      <img
+        src={image}
+        alt=""
+      />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 }
-
-const Image = () => <img src="https://m.media-amazon.com/images/I/71jltELj4YL._AC_UY327_FMwebp_QL65_.jpg" alt="" />;
-
-const Title = () => <h1>The Encompassing Epistle: Al-Risalah al-Jami'ah</h1>;
-
-const Author = () => <h4 style={{color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>Aḥmed ibn Zayn Al-Ḥabashī</h4>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<BookList />);
